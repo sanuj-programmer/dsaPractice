51228543ac4bc72ba942dc61d77fb028b8ecc74d
Sanuj kumar singh <120162867+sanuj-programmer@users.noreply.github.com>
2025-06-18 11:47:41 +0530

    new commit

--- Array/rightRotatebyD.cpp
+++ Array/rightRotatebyD.cpp
@@ -1,6 +1,6 @@
-// #include <iostream>   //Brute
+// #include <iostream>  //Brute
 // using namespace std;
-// void Rotatetoleft(int arr[], int n, int k)
+// void Rotatetoright(int arr[], int n, int k)
 // {
 //   if (n == 0)
 //     return;
@@ -8,17 +8,17 @@
 //   if (k > n)
 //     return;
 //   int temp[k];
-//   for (int i = 0; i < k; i++)
-//   {
-//     temp[i] = arr[i];
-//   }
-//   for (int i = 0; i < n - k; i++)
-//   {
-//     arr[i] = arr[i + k];
-//   }
 //   for (int i = n - k; i < n; i++)
 //   {
-//     arr[i] = temp[i - n + k];
+//     temp[i - n + k] = arr[i];
+//   }
+//   for (int i = n - k - 1; i >= 0; i--)
+//   {
+//     arr[i + k] = arr[i];
+//   }
+//   for (int i = 0; i < k; i++)
+//   {
+//     arr[i] = temp[i];
 //   }
 // }
 // int main()
@@ -26,8 +26,8 @@
 //   int n = 7;
 //   int arr[] = {1, 2, 3, 4, 5, 6, 7};
 //   int k = 2;
-//   Rotatetoleft(arr, n, k);
-//   cout << "After Rotating the elements to left " << endl;
+//   Rotatetoright(arr, n, k);
+//   cout << "After Rotating the elements to right " << endl;
 //   for (int i = 0; i < n; i++)
 //   {
 //     cout << arr[i] << " ";
@@ -36,7 +36,7 @@
 // }
 
 // Output:
-// After Rotating the elements to left 3 4 5 6 7 1 2
+// After Rotating the elements to right 6 7 1 2 3 4 5
 // Time Complexity: O(n)
 // Space Complexity: O(k) since k array element needs to be stored in temp array
 
@@ -46,7 +46,7 @@
 
 
 
-#include <iostream>   //Optm
+#include <iostream>    //Optm
 using namespace std;
 // Function to Reverse the array
 void Reverse(int arr[], int start, int end)
@@ -60,13 +60,13 @@ void Reverse(int arr[], int start, int end)
     end--;
   }
 }
-// Function to Rotate k elements to left
-void Rotateeletoleft(int arr[], int n, int k)
+// Function to Rotate k elements to right
+void Rotateeletoright(int arr[], int n, int k)
 {
-  // Reverse first k elements
-  Reverse(arr, 0, k - 1);
-  // Reverse last n-k elements
-  Reverse(arr, k, n - 1);
+  // Reverse first n-k elements
+  Reverse(arr, 0, n - k - 1);
+  // Reverse last k elements
+  Reverse(arr, n - k, n - 1);
   // Reverse whole array
   Reverse(arr, 0, n - 1);
 }
@@ -75,8 +75,8 @@ int main()
   int arr[] = {1, 2, 3, 4, 5, 6, 7};
   int n = 7;
   int k = 2;
-  Rotateeletoleft(arr, n, k);
-  cout << "After Rotating the k elements to left ";
+  Rotateeletoright(arr, n, k);
+  cout << "After Rotating the k elements to right ";
   for (int i = 0; i < n; i++)
     cout << arr[i] << " ";
   cout << endl;
@@ -84,6 +84,6 @@ int main()
 }
 
 // Output:
-// After Rotating the k elements to left 3 4 5 6 7 1 2
+// After Rotating the k elements to right 6 7 1 2 3 4 5
 // Time Complexity - O(N) where N is the number of elements in an array
 // Space Complexity - O(1) since no extra space is required
