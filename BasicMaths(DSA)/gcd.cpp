dfad87b169ac0d268d37dca8ba6eeacafa5dcab5
Sanuj <sanuj@Sanujs-MacBook-Air.local>
2025-07-19 21:00:17 +0530

    new commit

--- BasicMaths(DSA)/gcd.cpp
+++ BasicMaths(DSA)/gcd.cpp
@@ -40,38 +40,3 @@
 //     return 0;
 // }
 
-
-// #include <iostream>
-// using namespace std;
-
-// int main() {
-//     int n1, n2;
-//     cin >> n1 >> n2;
-
-//     int gcd = 1;
-
-//     for (int i = min(n1, n2); i >= 1; i--) {
-//         if (n1 % i == 0 && n2 % i == 0) {
-//             gcd = i;
-//             break;
-//         }
-//     }
-
-//     cout << "GCD: " << gcd << endl;
-
-//     return 0;
-// }
-
-
-#include <bits/stdc++.h>
-
-
-int main() {
-    int n1, n2;
-    cout << "Enter n1: ";
-    cin >> n1;
-    cout << "Enter n2: ";
-    cin >> n2;
-    cout << "sum: " << n1+n2 << endl;
-    return 0;
-}
