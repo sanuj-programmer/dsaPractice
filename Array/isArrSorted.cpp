0681390a9a3a1b2c0a5fae7fd2becc51e076bddd
Sanuj Kumar Singh <sanujkumarsingh12@gmail.com>
2025-07-19 21:15:18 +0530

    commit

--- Array/isArrSorted.cpp
+++ Array/isArrSorted.cpp
@@ -29,23 +29,23 @@
 
 
 
-#include<bits/stdc++.h>
-using namespace std;
+// #include<bits/stdc++.h>
+// using namespace std;
 
-bool isSorted(int arr[], int n) {
-  for (int i = 1; i < n; i++) {
-    if (arr[i] < arr[i - 1])
-      return false;
-  }
+// bool isSorted(int arr[], int n) {
+//   for (int i = 1; i < n; i++) {
+//     if (arr[i] < arr[i - 1])
+//       return false;
+//   }
 
-  return true;
-}
+//   return true;
+// }
 
-int main() {
+// int main() {
 
-  int arr[] = {1, 2, 3, 4, 5}, n = 5;
+//   int arr[] = {1, 2, 3, 4, 5}, n = 5;
 
-  printf("%s", isSorted(arr, n) ? "True" : "False");
+//   printf("%s", isSorted(arr, n) ? "True" : "False");
 
-}
+// }
 
