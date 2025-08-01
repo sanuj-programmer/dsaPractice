5f39855f66e8258562221132146400cc9182c551
Sanuj Kumar Singh <sanujkumarsingh12@gmail.com>
2025-08-01 21:34:58 +0530

    New commit

---
+++ Array/longestSubArrSumPN.cpp
@@ -0,0 +1,120 @@
+// #include <iostream>      // Naive-> Brute-force approach
+// #include <vector>
+
+// using namespace std;
+
+// int getLongestSubarray(vector<int>& a, int k) {
+//     int n = a.size(); // size of the array.
+
+//     int len = 0;
+//     for (int i = 0; i < n; i++) { // starting index
+//         for (int j = i; j < n; j++) { // ending index
+//             // add all the elements of
+//             // subarray = a[i...j]:
+//             int s = 0;
+//             for (int K = i; K <= j; K++) {
+//                 s += a[K];
+//             }
+
+//             if (s == k)
+//                 len = max(len, j - i + 1);
+//         }
+//     }
+//     return len;
+// }
+
+// int main()
+// {
+//     vector<int> a = { -1, 1, 1};
+//     int k = 1;
+//     int len = getLongestSubarray(a, k);
+//     cout << "The length of the longest subarray is: " << len << "\n";
+//     return 0;
+// }
+
+
+
+
+
+// #include <iostream>      // Better-> Using two loops
+// #include <vector>
+// using namespace std;
+
+// int getLongestSubarray(vector<int>& a, int k) {
+//     int n = a.size(); // size of the array.
+
+//     int len = 0;
+//     for (int i = 0; i < n; i++) { // starting index
+//         int s = 0;
+//         for (int j = i; j < n; j++) { // ending index
+//             // add the current element to
+//             // the subarray a[i...j-1]:
+//             s += a[j];
+
+//             if (s == k)
+//                 len = max(len, j - i + 1);
+//         }
+//     }
+//     return len;
+// }
+
+// int main()
+// {
+//     vector<int> a = { -1, 1, 1};
+//     int k = 1;
+//     int len = getLongestSubarray(a, k);
+//     cout << "The length of the longest subarray is: " << len << "\n";
+//     return 0;
+// }
+
+
+
+
+
+
+#include <iostream>      // Optimal-> Using Hashing
+#include <vector>
+#include <map>
+using namespace std;
+
+int getLongestSubarray(vector<int>& a, int k) {
+    int n = a.size(); // size of the array.
+
+    map<int, int> preSumMap;
+    int sum = 0;
+    int maxLen = 0;
+    for (int i = 0; i < n; i++) {
+        //calculate the prefix sum till index i:
+        sum += a[i];
+
+        // if the sum = k, update the maxLen:
+        if (sum == k) {
+            maxLen = max(maxLen, i + 1);
+        }
+
+        // calculate the sum of remaining part i.e. x-k:
+        int rem = sum - k;
+
+        //Calculate the length and update maxLen:
+        if (preSumMap.find(rem) != preSumMap.end()) {
+            int len = i - preSumMap[rem];
+            maxLen = max(maxLen, len);
+        }
+
+        //Finally, update the map checking the conditions:
+        if (preSumMap.find(sum) == preSumMap.end()) {
+            preSumMap[sum] = i;
+        }
+    }
+
+    return maxLen;
+}
+
+int main()
+{
+    vector<int> a = { -1, 1, 1};
+    int k = 1;
+    int len = getLongestSubarray(a, k);
+    cout << "The length of the longest subarray is: " << len << "\n";
+    return 0;
+}
